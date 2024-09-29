e9e651c95d912d7b8e5068a1cce651ee84698c15
vladcherpak <cherpakvl@gmail.com>
2024-09-29 19:41:02 +0300

    burger menu

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,25 +1,42 @@
 import css from "./style.module.css";
-import React from 'react'
+import React, { useState } from "react";
 import bg from "../../static/bgimage1.jpg";
 import logo from "../../static/logo-sos.svg";
 
 
+// menu__btn.addEventListener("click", () => {
+//   menuBtn.classList.toggle("active");
+//   list.classList.toggle("active");
+// });
+
 
 const Hero = () => {
 
+  const [show,setShow]=useState(false)
+  const menuHandler =()=>{
+    setShow(!show)
+    console.log(show);
+    
+  }
   return (
-    <section className={css.section} style={{backgroundImage:`url(${bg})`}}>
+    <section className={css.section} style={{ backgroundImage: `url(${bg})` }}>
       <div className={css.hero}>
-
-        {/* <img className={css.logo}  alt={"image"} src={logo} />
-        <ul className={css.list}>
+      <button onClick={()=>menuHandler()} className={show ? css.menu__btn + " " + css.active:css.menu__btn}>
+        <span className={css.bar}></span>
+        <span className={css.bar}></span>
+        <span className={css.bar}></span>
+      </button>
+        <img className={css.logo} alt={"image"} src={logo} />
+        <ul className={show ? css.list + " " + css.active:css.list}>
           <li className={css.item}> Main</li>
           <li className={css.item}> About</li>
           <li className={css.item}> Game features</li>
           <li className={css.item}> System requirements</li>
           <li className={css.item}> Quotes</li>
-        </ul> */}
+        </ul>
+
       </div>
+      
       <div className={css.main}>
         <h1 className={css.title}>SURVIVE AT ALL COSTS</h1>
         <p className={css.text}>Experience new social battle royale game</p>
